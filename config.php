e0fbb04f108484d1f4c4f8e6405afbb5dd38d1ba
Juan Avendaño <56646543+Adrephos@users.noreply.github.com>
2023-11-07 23:46:10 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = '10.0.133.132';
+$CFG->dbname    = 'moodledb';
+$CFG->dbuser    = 'moodle';
+$CFG->dbpass    = 'password';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'http://lms.adrephos.me';
+$CFG->dataroot  = '/var/www/html/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
